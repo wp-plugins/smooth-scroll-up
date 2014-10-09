bbfc08bdffb38a0d178dc94d8d61fc07608e26f2
kouratoras <kouratoras@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-09 14:20:19 +0000

    Minor bug fix
    git-svn-id: https://plugins.svn.wordpress.org/smooth-scroll-up/trunk@1004531 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -9,8 +9,8 @@
   Tags: back to top, scroll to top, scroll, scroll top, scroll back to top, scroll up, arrow, link to top, back to top, smooth scroll, top, up, back, navigation
   Requires at least: 3.2
   Tested up to: 4.1
-  Stable tag: 0.8.5
-  Version: 0.8.5
+  Stable tag: 0.8.6
+  Version: 0.8.6
   License: GPLv2 or later
   Description: Smooth Scroll Up is a lightweight plugin that creates a customizable "Scroll to top / Back to top" feature in any post/page of your WordPress website.
 
@@ -93,7 +93,7 @@ class ScrollUp {
 		
 		$scrollup_show = ($this->settings['scrollup_show'] ? $this->settings['scrollup_show'] : '0');
 
-		if ($scrollup_show == "1" || $scrollup_show == "0" && (!is_home() || !is_front_page())) {
+		if ($scrollup_show == "1" || ($scrollup_show == "0" && !(is_home() || is_front_page()))) {
 			
 			//Fetch options
 			$scrollup_type = ($this->settings['scrollup_type'] ? $this->settings['scrollup_type'] : 'tab');
